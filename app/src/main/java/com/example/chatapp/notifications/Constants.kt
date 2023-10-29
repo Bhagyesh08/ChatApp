24d05a72e1b60c162fd2c8fed6504a0e7060eb87
Bhagyesh08 <csbhagyeshsingh@gmail.com>
2023-10-29 17:29:27 +0530

    ChatApp

--- app/src/main/java/com/example/chatapp/notifications/Constants.kt
+++ app/src/main/java/com/example/chatapp/notifications/Constants.kt
@@ -3,7 +3,7 @@ package com.example.chatapp.notifications
 class Constants {
     companion object {
         const val BASE_URL = "https://fcm.googleapis.com"
-        const val SERVER_KEY = "{inserserverkey}"
+        const val SERVER_KEY = "AAAANIBG9rc:APA91bHUe0ewOq5CgXBOmI11Z1_tQxyiek-wsOtUkv48aZeZT2DcgTh6R2sdPO21TMcXDOnDKNSTVg8KQnuiYn4SBK8Ax0qB24Gr_7Zs4ycObABGQ62DsZoRhYONelbg0Whsvh-b5rNy"
         const val CONTENT_TYPE = "application/json"
     }
 }
